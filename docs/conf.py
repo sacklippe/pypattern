0bd3923a632e6f6e3221a20069abd9f5cbb7540c
Jonas Kleppich <kleppo@Jonass-MBP.fritz.box>
2022-11-03 00:38:14 +0100

    doc: add duration extension

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,11 @@ release = "0.1.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "myst_parser"]
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.duration",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
