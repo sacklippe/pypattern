13c8eb501ac4a834000556146080c9dc38c748da
Jonas Kleppich <kleppo@Jonass-MBP.fritz.box>
2022-11-04 21:11:13 +0100

    ci: add build workflow to auto-build the github page
    This also changes the theme to python-docs-theme.

--- docs/conf.py
+++ docs/conf.py
@@ -28,5 +28,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "furo"
+html_theme = "python_docs_theme"
 html_static_path = ["_static"]
